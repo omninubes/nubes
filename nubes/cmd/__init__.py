8f094e1c3d4a64942cadf5603ce5b23706381fac
Brandon Logan <brandon.logan@rackspace.com>
2016-12-30 02:25:56 -0600

    Make crude CLI commands work
    This is mainly as an example to show what it can look like.

--- nubes/cmd/__init__.py
+++ nubes/cmd/__init__.py
@@ -1,5 +1,25 @@
-import openstack
+import argparse
+
+from nubes import dispatcher
 
 
 def main():
-    print("Hello Clouds!")
+    parser = argparse.ArgumentParser(description='Universal IaaS CLI')
+    parser.add_argument('connector', help='IaaS Name')
+    parser.add_argument('resource', help='Resource to perform action')
+    parser.add_argument('action', help='Action to perform on resource')
+    parser.add_argument('--auth-url')
+    parser.add_argument('--username')
+    parser.add_argument('--password')
+    parser.add_argument('--project-name')
+    args = parser.parse_args()
+    dispatch = dispatcher.Dispatcher(args.connector, args.auth_url,
+                                     args.username, args.password,
+                                     args.project_name)
+    resource = args.resource
+    if args.action == 'list':
+        # make plural
+        resource = args.resource + 's'
+
+    method_name = '_'.join([args.action, resource])
+    return getattr(dispatch, method_name)()
