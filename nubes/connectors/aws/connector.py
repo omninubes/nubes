3856b48af3e83f49a66c0c29b81e0a80ad3248d9
Carlos 'Legionarius' Martinez <carlos.martinez@rackspace.com>
2017-01-30 16:02:49 -0600

    Move client and resource to __init__
    * moved the calls to create the ec2 session resource session client
      to the init

--- nubes/connectors/aws/connector.py
+++ nubes/connectors/aws/connector.py
@@ -11,26 +11,26 @@ class AWSConnector(base.BaseConnector):
             aws_secret_access_key=aws_secret_access_key,
             region_name=region_name)
 
+        self.ec2_resource = self.connection.resource("ec2")
+        self.ec2_client = self.connection.client("ec2")
+
     @classmethod
     def name(cls):
         return "aws"
 
     def create_server(self, image_id, min_count, max_count, **kwargs):
-        ec2_resource = self.connection.resource("ec2")
-        server = ec2_resource.create_instances(ImageId=image_id,
-                                               MinCount=min_count,
-                                               MaxCount=max_count,
-                                               **kwargs)
+        server = self.ec2_resource.create_instances(ImageId=image_id,
+                                                    MinCount=min_count,
+                                                    MaxCount=max_count,
+                                                    **kwargs)
         return server
 
     def list_servers(self):
-        ec2_client = self.connection.client("ec2")
-        desc = ec2_client.describe_instances()
+        desc = self.ec2_client.describe_instances()
         return desc
 
     def delete_server(self, instance_id):
-        ec2_resource = self.connection.resource("ec2")
-        ec2_resource.instances.filter(
+        self.ec2_resource.instances.filter(
             InstanceIds=[instance_id]).stop()
-        ec2_resource.instances.filter(
+        self.ec2_resource.instances.filter(
             InstanceIds=[instance_id]).terminate()
